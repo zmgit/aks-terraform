952b5d808ba2dd76d0307a939d3119df7a152623
Mykhailo Zahlada <zmpbox@gmail.com>
2021-03-08 15:25:12 +0100

    bump versions

--- versions.tf
+++ versions.tf
@@ -1,12 +1,11 @@
 
-
 terraform {
-  required_version = ">= 0.13"
+  required_version = ">= 0.14"
 
   required_providers {
-    azurerm    = ">=2.37.0"
-    azuread    = ">=1.1.0"
-    kubernetes = "= 1.9.0"
-    helm       = "= 0.10.2"
+    azurerm    = ">=2.47.0"
+    azuread    = ">=1.3.0"
+    kubernetes = ">= 1.11.0"
+    helm       = ">= 1.1.2"
   }
 }
